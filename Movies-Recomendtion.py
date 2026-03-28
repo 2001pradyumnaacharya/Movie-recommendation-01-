70a58537932b9357d425c738f2d46f7093306111
Pradyumna <Pradyumnaacharya406@gmail.com>
2026-03-28 20:41:11 +0530

    UI started

--- Movies-Recomendtion.py
+++ Movies-Recomendtion.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 import ast 
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.metrics.pairwise import cosine_similarity
 
+
+ps = PorterStemmer()
+cv = CountVectorizer(max_features=5000,stop_words='english')
 # Creating dataFrame of movies
 movies_data_df = pd.read_csv(r"/home/pradyumna/MLProjects/Movie-recommendation-01-/tmdb_5000_movies.csv")
 credts_data_df = pd.read_csv(r"/home/pradyumna/MLProjects/Movie-recommendation-01-/tmdb_5000_credits.csv")
@@ -30,9 +36,74 @@ def convert(obj):
     return temp
 
 
+def stem(text):
+    y = []
+    for i in text.split():
+        y.append(ps.stem(i))
+    
+    return " ".join(y)
+
+
+
+
+def convert3(obj):
+    temp = []
+    count = 0
+    for j in ast.literal_eval(obj):
+        # print(j['name'])
+        if not count == 3:
+            temp.append(j['name'])
+            count +=1
+    return temp
+
+
+def filtter_dir(obj):
+    temp = []
+
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            temp.append(i['name'])
+            break
+    
+    return temp
+
+
+
+
 filltred_df['genres'] = filltred_df['genres'].apply(convert)
 filltred_df['keywords'] = filltred_df['keywords'].apply(convert)
+filltred_df['cast'] = filltred_df['cast'].apply(convert3)
+filltred_df['crew'] = filltred_df['crew'].apply(filtter_dir)
+#print(filltred_df['keywords'])
+filltred_df['crew'] = filltred_df['crew'].apply(lambda x : [i.replace(" ", "") for i in x] )
+filltred_df['cast'] = filltred_df['cast'].apply(lambda x : [i.replace(" ", "") for i in x] )
+filltred_df['keywords'] = filltred_df['keywords'].apply(lambda x : [i.replace(" ", "") for i in x] )
+filltred_df['genres'] = filltred_df['genres'].apply(lambda x : [i.replace(" ", "") for i in x] )
+filltred_df['overview'] = filltred_df['overview'].apply(lambda x: x.split())
 
-print(filltred_df['keywords'])
+
+
+filltred_df['Tags'] = filltred_df['overview'] + filltred_df['genres'] + filltred_df['keywords'] + filltred_df['cast'] + filltred_df['crew'] 
+
+filltred_df['Tags'] = filltred_df['Tags'].apply(lambda x : " ".join(x))
+filltred_df['Tags'] = filltred_df['Tags'].apply(lambda x : x.lower())
+filltred_df['Tags'] = filltred_df['Tags'].apply(stem)
+vectors = cv.fit_transform(filltred_df['Tags']).toarray()
+simileraty = cosine_similarity(vectors).shape
+
+
+
+def recommend(movie):
+    movie_index = filltred_df[filltred_df['title'] == movie].index[0] 
+    distances = simileraty[movie_index]
+    movies_list = sorted(list(enumerate(simileraty[distances])),reverse=True,key=lambda x:x[1])[1:6]
+    for i in movies_list:
+        print(filltred_df.iloc[i[0]].title)  
+      
+    # cv.get_feature_names_out()
+# print(cv.get_feature_names_out())
+
+# print(vectors[0])
+
 
     
--- README.md
+++ README.md
@@ -42,3 +42,20 @@ genres      0<br>
 cast        0<br>
 crew        0<br>
 sence <b> OverView</b> is very important column and i cant afford it to be NULL so sence the count of null value is not much i would like to drop those rows or say that data 
+
+Now sence the Keyword and Overview is solved we should now go for column <b>cast</b> it looks something like this now <br>
+{"cast_id": 242, "character": "Jake Sully", "credit_id": "5602a8a7c3a3685532001c9a", "gender": 2, "id": 65731, "name": "Sam Worthington", "order": 0}
+
+we are just appending the names of the actor's top 3 one from the cast and appending in the list so the list may look some thing like this<br>
+['Sam Worthington', 'Zoe Saldana', 'Sigourney Weaver']
+ 
+Now the column crew should be prossed and the way i am going to prosses this column data is i am going to see the job weather the job is listed as Director if yes i am going to append it else not.<br>
+
+one last step of the data proccessing is to remove the spaces form the words in the column such that there will be no Ambiguity and <b>Overview</b> is the column which is in the string should be in the list like any other columns in the dataframe so i converted in the list so that i can concat ithem all to make <b>Tag</b> column.
+
+## Next step of data-Prossing
+Sence the we cleard the shit in our data its time to male the data in right formate so that its becomes easy to prosses lets concat the columns and add a new column in the df called <b>Tag</b> and remove all other column , and convert the column back to string<br>
+Just to make the data easy for the vectorization i would convert it all the <b>Tags</b> column data to the smaller
+
+## Vectorization
+The whole point here is to convert the each sentance of the tages to the vector, and how are we going to do it using concept <b>Bag of Words</b> <a>Bag of Words (BoW) is a fundamental technique in Natural Language Processing (NLP) that converts text into numerical features for machine learning models</a> In vectorization we will remove the <b>Stop Words</b> and we are going to do it using Sklearn to do vectorization 
